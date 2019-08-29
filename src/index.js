a77e6074238ddd2d2a6517f1d64253d47c7aaa07
Abby Fang <abbycareer@hotmail.com>
2019-08-29 22:39:25 +1200

    Update index.js
    change name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Welcome to NZ IT Training</h1>
-<p>I'm Cody.</p>
+<p>I'm abby.</p>
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
